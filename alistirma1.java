3cc8c507deedf67c70dbfd9af9ed3917eb6321d2
y-berber <100419372+y-berber@users.noreply.github.com>
2022-04-05 21:47:39 +0300

    Update alistirma1.java

--- alistirma1.java
+++ alistirma1.java
@@ -3,18 +3,17 @@ package ugurHoca;
 public class alistirma1 {
 
 	public static void main(String[] args) {
-	System.out.println();
-	System.out.println("           ******           *************        **********");
-	System.out.println("           **    **                **            **");
-	System.out.println("           **     **               **            **");
-	System.out.println("           **      **              **            **");
-	System.out.println("           **      **              **            ********");
-	System.out.println("           **      **       **     **            **");
-	System.out.println("           **     **         **    **            **");
-	System.out.println("           **    **           **  **             **");
-	System.out.println("           *****               ****              **********");
-	System.out.println();
 
+	System.out.println();
+	System.out.println("    **      **      ***         **   **	    **      **   *******");
+	System.out.println("     **    **      ** **        **   **	    **      **   ********");
+	System.out.println("      **  **      **   **       **  **      **      **   **     **");
+	System.out.println("        **       **     **      ** **       **      **   **     **");
+	System.out.println("        **      ***********     ****        **      **   ********");
+	System.out.println("        **      ***********     *****       **      **   *******");
+	System.out.println("        **      **       **     **  **      **      **   **");
+	System.out.println("        **      **       **     **   **     **********   **");
+	System.out.println("        **      **       **     **   **     **********   **");
+	System.out.println();
 	}
-
 }
